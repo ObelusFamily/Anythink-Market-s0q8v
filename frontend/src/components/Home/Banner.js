5f46a070eeeeef749c9daa1084c865e019e95d76
Pat Walsh <pmwals09@gmail.com>
2022-07-27 23:15:17 -0400

    Add search UI functionality
    - Adds input field to Banner component
    - Adds search logic to Banner component on input change
    - Adds text filter request logic to agent
    - Adds redux plumbing in between:
      - dispatch function
      - reducer case

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent.js";
 
-const Banner = () => {
+const Banner = ({ onTextFilter }) => {
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    const filterPromise = agent.Items.byText;
+    if (searchText.length > 2) {
+      agent.Items.byText(searchText).then((res) => {
+        onTextFilter(filterPromise, res);
+      });
+    } else {
+      agent.Items.byText().then((res) => {
+        onTextFilter(filterPromise, res);
+      });
+    }
+  }, [searchText, onTextFilter]);
+
+  function handleChange(e) {
+    setSearchText(e.target.value);
+  }
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <input
+            id="search-box"
+            placeholder="What is it that you truly desire?"
+            value={searchText}
+            onChange={handleChange}
+          />
           <span> the cool stuff.</span>
         </div>
       </div>
