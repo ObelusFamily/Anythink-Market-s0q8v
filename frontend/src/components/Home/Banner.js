8b226e0a6ff1c8a858dbc99fc9245811bfa0c751
Pat Walsh <pmwals09@gmail.com>
2022-07-28 00:01:49 -0400

    Add Search UI styling

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -26,15 +26,26 @@ const Banner = ({ onTextFilter }) => {
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
-        <div>
-          <span id="get-part">A place to get</span>
-          <input
-            id="search-box"
-            placeholder="What is it that you truly desire?"
-            value={searchText}
-            onChange={handleChange}
-          />
-          <span> the cool stuff.</span>
+        <div className="row align-items-center">
+          <span className="col-4 text-right" id="get-part">
+            A place to get
+          </span>
+          <div className="input-group col-4">
+            <input
+              type="text"
+              className="form-control border-right-0"
+              id="search-box"
+              placeholder="What is it that you truly desire?"
+              value={searchText}
+              onChange={handleChange}
+            />
+            <span className="input-group-append bg-white rounded">
+              <span className="input-group-text bg-transparent">
+                <i class="bi bi-search" style={{ color: "purple" }}></i>
+              </span>
+            </span>
+          </div>
+          <span className="col-4 text-left"> the cool stuff.</span>
         </div>
       </div>
     </div>
